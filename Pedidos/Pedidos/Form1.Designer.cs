9708ec419529b90c9cbe9dd96e6019af59c33e34
Juan Carlos Reyes <ingreyesguerrero@gmail.com>
2023-08-14 17:30:51 -0500

    Se agrega diseño ventana principal
    Se agrega diseño ventana principal

--- Pedidos/Pedidos/Form1.Designer.cs
+++ Pedidos/Pedidos/Form1.Designer.cs
@@ -28,12 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            statusStrip1 = new StatusStrip();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            menuStrip1 = new MenuStrip();
+            inicioToolStripMenuItem = new ToolStripMenuItem();
+            directorioToolStripMenuItem = new ToolStripMenuItem();
+            operacionesToolStripMenuItem = new ToolStripMenuItem();
+            productosToolStripMenuItem = new ToolStripMenuItem();
+            mostrarToolStripMenuItem = new ToolStripMenuItem();
+            agregarToolStripMenuItem = new ToolStripMenuItem();
+            inventarioToolStripMenuItem = new ToolStripMenuItem();
+            ayudaToolStripMenuItem = new ToolStripMenuItem();
+            dtgPrincipal = new DataGridView();
+            btnBuscar = new Button();
+            txtProducto = new TextBox();
+            label1 = new Label();
+            btnVerVenta = new Button();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dtgPrincipal).BeginInit();
+            SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new Point(0, 698);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(923, 22);
+            statusStrip1.TabIndex = 0;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { inicioToolStripMenuItem, directorioToolStripMenuItem, operacionesToolStripMenuItem, productosToolStripMenuItem, inventarioToolStripMenuItem, ayudaToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(923, 24);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // inicioToolStripMenuItem
+            // 
+            inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            inicioToolStripMenuItem.Size = new Size(48, 20);
+            inicioToolStripMenuItem.Text = "Inicio";
+            // 
+            // directorioToolStripMenuItem
+            // 
+            directorioToolStripMenuItem.Name = "directorioToolStripMenuItem";
+            directorioToolStripMenuItem.Size = new Size(71, 20);
+            directorioToolStripMenuItem.Text = "Directorio";
+            // 
+            // operacionesToolStripMenuItem
+            // 
+            operacionesToolStripMenuItem.Name = "operacionesToolStripMenuItem";
+            operacionesToolStripMenuItem.Size = new Size(85, 20);
+            operacionesToolStripMenuItem.Text = "Operaciones";
+            // 
+            // productosToolStripMenuItem
+            // 
+            productosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { mostrarToolStripMenuItem, agregarToolStripMenuItem });
+            productosToolStripMenuItem.Name = "productosToolStripMenuItem";
+            productosToolStripMenuItem.Size = new Size(73, 20);
+            productosToolStripMenuItem.Text = "Productos";
+            // 
+            // mostrarToolStripMenuItem
+            // 
+            mostrarToolStripMenuItem.Name = "mostrarToolStripMenuItem";
+            mostrarToolStripMenuItem.Size = new Size(180, 22);
+            mostrarToolStripMenuItem.Text = "Mostrar";
+            mostrarToolStripMenuItem.Click += mostrarToolStripMenuItem_Click;
+            // 
+            // agregarToolStripMenuItem
+            // 
+            agregarToolStripMenuItem.Name = "agregarToolStripMenuItem";
+            agregarToolStripMenuItem.Size = new Size(180, 22);
+            agregarToolStripMenuItem.Text = "Agregar";
+            agregarToolStripMenuItem.Click += agregarToolStripMenuItem_Click;
+            // 
+            // inventarioToolStripMenuItem
+            // 
+            inventarioToolStripMenuItem.Name = "inventarioToolStripMenuItem";
+            inventarioToolStripMenuItem.Size = new Size(72, 20);
+            inventarioToolStripMenuItem.Text = "Inventario";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            ayudaToolStripMenuItem.Size = new Size(53, 20);
+            ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // dtgPrincipal
+            // 
+            dtgPrincipal.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgPrincipal.Location = new Point(12, 81);
+            dtgPrincipal.Name = "dtgPrincipal";
+            dtgPrincipal.RowTemplate.Height = 25;
+            dtgPrincipal.Size = new Size(899, 605);
+            dtgPrincipal.TabIndex = 4;
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Location = new Point(587, 52);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(75, 23);
+            btnBuscar.TabIndex = 5;
+            btnBuscar.Text = "Buscar";
+            btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtProducto
+            // 
+            txtProducto.Location = new Point(89, 52);
+            txtProducto.Name = "txtProducto";
+            txtProducto.Size = new Size(492, 23);
+            txtProducto.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Producto";
+            // 
+            // btnVerVenta
+            // 
+            btnVerVenta.Location = new Point(836, 52);
+            btnVerVenta.Name = "btnVerVenta";
+            btnVerVenta.Size = new Size(75, 23);
+            btnVerVenta.TabIndex = 8;
+            btnVerVenta.Text = "Ver Venta";
+            btnVerVenta.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(923, 720);
+            Controls.Add(btnVerVenta);
+            Controls.Add(label1);
+            Controls.Add(txtProducto);
+            Controls.Add(btnBuscar);
+            Controls.Add(dtgPrincipal);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dtgPrincipal).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private StatusStrip statusStrip1;
+        private ContextMenuStrip contextMenuStrip1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem inicioToolStripMenuItem;
+        private ToolStripMenuItem directorioToolStripMenuItem;
+        private ToolStripMenuItem operacionesToolStripMenuItem;
+        private ToolStripMenuItem productosToolStripMenuItem;
+        private ToolStripMenuItem mostrarToolStripMenuItem;
+        private ToolStripMenuItem agregarToolStripMenuItem;
+        private ToolStripMenuItem inventarioToolStripMenuItem;
+        private ToolStripMenuItem ayudaToolStripMenuItem;
+        private DataGridView dtgPrincipal;
+        private Button btnBuscar;
+        private TextBox txtProducto;
+        private Label label1;
+        private Button btnVerVenta;
     }
 }
--- Pedidos/Pedidos/Form1.cs
+++ Pedidos/Pedidos/Form1.cs
@@ -1,3 +1,15 @@
+using Microsoft.VisualBasic.Logging;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 namespace Pedidos
 {
     public partial class Form1 : Form
@@ -5,6 +17,39 @@ namespace Pedidos
         public Form1()
         {
             InitializeComponent();
+
+            dtgPrincipal.Columns.Add("id", "Id");
+            dtgPrincipal.Columns.Add("nombre", "Nombre");
+            dtgPrincipal.Columns.Add("unidad", "Unidad");
+            dtgPrincipal.Columns.Add("disponible", "Disponible");
+            dtgPrincipal.Columns.Add("precio", "Precio");
+            dtgPrincipal.Columns[0].Width = 50;
+            dtgPrincipal.Columns[1].Width = 400;
+            dtgPrincipal.Columns[2].Width = 100;
+            dtgPrincipal.Columns[3].Width = 100;
+            dtgPrincipal.Columns[4].Width = 100;
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mostrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProductsWindow pw = new ProductsWindow();
+            pw.ShowDialog();
+        }
+
+        private void agregarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newProduct np = new newProduct();
+            np.ShowDialog();
         }
     }
 }
