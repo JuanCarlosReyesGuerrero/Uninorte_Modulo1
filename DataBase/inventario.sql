8d769b351cd398848b06dfbf695172bba2d7bfaa
Juan Carlos Reyes <ingreyesguerrero@gmail.com>
2023-08-14 22:39:23 -0500

    Modifica data de la base de datos y diccionario de datos
    Modifica data de la base de datos y diccionario de datos

--- DataBase/inventario.sql
+++ DataBase/inventario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-08-2023 a las 00:14:48
+-- Tiempo de generación: 15-08-2023 a las 05:38:31
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -153,6 +153,16 @@ CREATE TABLE `product` (
   `is_active` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `product`
+--
+
+INSERT INTO `product` (`id`, `image`, `barcode`, `name`, `description`, `inventary_min`, `price_in`, `price_out`, `unit`, `presentation`, `user_id`, `category_id`, `created_at`, `is_active`) VALUES
+(1, NULL, '', 'Blanqueador sin fragancia EKONO SIN REF', '', 10, 2800, 3140, 'Galon', '', NULL, NULL, '0000-00-00 00:00:00', 1),
+(2, NULL, '', 'Aceite Vegetal FRESCAMPO 3000 ml', '', 10, 20000, 21980, 'Botella ', '', NULL, NULL, '0000-00-00 00:00:00', 1),
+(3, NULL, '', 'Lenteja FRESCAMPO 500 gr', '', 10, 1500, 2980, 'unidad', '', NULL, NULL, '0000-00-00 00:00:00', 1),
+(4, NULL, '', 'Huevo Rojo A X 30 SMN 30 und', '', 10, 14000, 15980, 'Panal', '', NULL, NULL, '0000-00-00 00:00:00', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -193,6 +203,36 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `name`, `lastname`, `username`, `email`, `password`, `image`, `is_active`, `is_admin`, `created_at`) VALUES
 (1, 'Administrador', '', NULL, 'admin', '90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad', NULL, 1, 1, '2023-08-14 17:10:25');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id` int(11) NOT NULL,
+  `code` int(11) NOT NULL DEFAULT 1,
+  `name` varchar(50) NOT NULL,
+  `unit` varchar(255) NOT NULL,
+  `cant` int(11) DEFAULT NULL,
+  `price` float NOT NULL,
+  `total` float NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`id`, `code`, `name`, `unit`, `cant`, `price`, `total`, `status`, `created_at`, `is_active`) VALUES
+(1, 2, 'Aceite Vegetal FRESCAMPO 3000 ml', 'Botella ', 2, 21980, 43960, 0, '0000-00-00 00:00:00', 1),
+(2, 4, 'Huevo Rojo A X 30 SMN 30 und', 'Panal', 3, 15980, 47940, 2, '0000-00-00 00:00:00', 1),
+(3, 2, 'Aceite Vegetal FRESCAMPO 3000 ml', 'Botella ', 3, 21980, 65940, 0, '0000-00-00 00:00:00', 1),
+(4, 4, 'Huevo Rojo A X 30 SMN 30 und', 'Panal', 5, 15980, 79900, 0, '0000-00-00 00:00:00', 1),
+(5, 4, 'Huevo Rojo A X 30 SMN 30 und', 'Panal', 3, 15980, 47940, 2, '0000-00-00 00:00:00', 1);
 
 --
 -- Índices para tablas volcadas
@@ -264,6 +304,12 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -307,7 +353,7 @@ ALTER TABLE `person`
 -- AUTO_INCREMENT de la tabla `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `sell`
@@ -320,6 +366,12 @@ ALTER TABLE `sell`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
